135cff176530c2699d4dfbc8ecb5993d9b64d5df
cool <niluherath@hotmail.com>
2024-06-26 20:14:44 +1200

    first push

---
+++ src/com.test/java/Login/LandingPageTest.java
@@ -0,0 +1,16 @@
+package Login;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+
+public class LandingPageTest extends TradeMeTest {
+
+
+    @Test
+    public void testLandingPage(){
+        System.out.println("testing");
+    }
+}
